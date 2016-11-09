aab3924ab4e3e837062998fc63c36efc2db5bb75
Baylor Rae <baylor@thecodedeli.com>
2016-11-09 16:32:30 -0600

    add Props#find_or_add_by

--- lib/playwright/props.rb
+++ lib/playwright/props.rb
@@ -1,4 +1,17 @@
 module Playwright
   class Props < Array
+    attr_accessor :include_query
+
+    def find_or_add_by(item)
+      self << item unless include?(item) || include_from_query?(item)
+      item
+    end
+
+    private
+
+    def include_from_query?(item)
+      include_query && self.any? { |v| include_query.call(v, item) }
+    end
+
   end
 end
--- spec/playwright/props_spec.rb
+++ spec/playwright/props_spec.rb
@@ -4,7 +4,36 @@ module Playwright
   describe Props do
 
     it "is an array" do
-      expect(Props.new).to be_kind_of(Array)
+      expect(subject).to be_kind_of(Array)
+    end
+
+    context "find_or_add_by" do
+      let(:users) { [double(:bob, name: "bob"), double(:alice, name: "alice"), double(:leo, name: "leo")] }
+
+      it "adds an item that doesn't exist" do
+        subject.find_or_add_by("one")
+        expect(subject).to eq(["one"])
+      end
+
+      it "doesn't add the item if already exists" do
+        %w[one two three].each { |i| subject << i }
+        subject.find_or_add_by("two")
+        expect(subject).to eq(%w[one two three])
+      end
+
+      it "returns the value being passed in" do
+        %w[one two three].each { |i| subject << i }
+        expect(subject.find_or_add_by("two")).to eq("two")
+      end
+
+      it "finds an item from an expression" do
+        users.each { |u| subject << u }
+
+        subject.include_query = Proc.new { |a, b| a.name == b.name }
+        subject.find_or_add_by(double(:duplicate_alice, name: "alice"))
+
+        expect(subject).to eq(users)
+      end
     end
 
   end
