8f9bff62197e6af3cd954d520f0683cf04bba3b2
Baylor Rae <baylor@thecodedeli.com>
2016-11-10 17:59:21 -0600

    add dsl for adding actors to stage

--- lib/playwright/stage.rb
+++ lib/playwright/stage.rb
@@ -1,5 +1,21 @@
 module Playwright
   class Stage
+    def method_missing(name, *args)
+      if @@actors.has_key?(name)
+        return @@actors[name]
+      end
+
+      super
+    end
+
+    def actors
+      @@actors.values
+    end
+
+    def self.actors(&block)
+      @@actors = ActorDSL.find(&block)
+    end
+
     def scenes
       @scenes ||= []
     end
@@ -11,6 +27,32 @@ module Playwright
     def self.prop_collection(name, &block)
       define_method name do
         Props.new(block)
+      end
+    end
+
+    private
+
+    class ActorDSL
+      attr_reader :actors
+
+      def initialize(context)
+        @context = context
+        @actors = {}
+      end
+
+      def method_missing(name, *args, &block)
+        @context.send(name, args, &block)
+      end
+
+      def self.find(&block)
+        context = eval("self", block.binding)
+        dsl = new(context)
+        dsl.instance_eval(&block)
+        dsl.actors
+      end
+
+      def actor(name, &block)
+        @actors[name] = yield
       end
     end
   end
--- spec/playwright/stage_spec.rb
+++ spec/playwright/stage_spec.rb
@@ -2,13 +2,37 @@ require "spec_helper"
 
 module Playwright
   describe Stage do
+    class UserFinder
+      def self.bob
+        @bob ||= Class.new
+      end
+
+      def self.alice
+        @alice ||= Class.new
+      end
+    end
 
     class ExampleStage < Stage
+      actors do
+        actor(:actor_1) { UserFinder.bob }
+        actor(:actor_2) { UserFinder.alice }
+      end
+
       prop_collection :shoes
       prop_collection(:pets) { |p| p.name }
     end
 
     subject { ExampleStage.new }
+
+    context "actors" do
+      it "returns available actors" do
+        expect(subject.actors).to eq([UserFinder.bob, UserFinder.alice])
+      end
+
+      it "adds an accessor for the actor" do
+        expect(subject.actor_1).to eq(UserFinder.bob)
+      end
+    end
 
     context "scenes" do
       it "has a collection of scenes" do
