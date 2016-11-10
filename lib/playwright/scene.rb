81fb4d67d2378c74317e737e28de96ef6ad709e2
Baylor Rae <baylor@thecodedeli.com>
2016-11-10 06:33:04 -0600

    add sender and receiver accessors

--- lib/playwright/scene.rb
+++ lib/playwright/scene.rb
@@ -1,4 +1,11 @@
 module Playwright
   class Scene < Struct.new(:sender, :receiver)
+    def self.sender_accessor(name)
+      define_method name, instance_method(:sender)
+    end
+
+    def self.receiver_accessor(name)
+      define_method name, instance_method(:receiver)
+    end
   end
 end
--- spec/playwright/scene_spec.rb
+++ spec/playwright/scene_spec.rb
@@ -2,5 +2,26 @@ require "spec_helper"
 
 module Playwright
   describe Scene do
+    let(:sender) { double(:sender) }
+    let(:receiver) { double(:receiver) }
+
+    class ExampleScene < Scene
+      sender_accessor :example_sender
+      receiver_accessor :example_receiver
+    end
+
+    subject { ExampleScene.new(sender, receiver) }
+
+    context "sender_accessor" do
+      it "aliases the sender method" do
+        expect(subject.example_sender).to eq(sender)
+      end
+    end
+
+    context "receiver_accessor" do
+      it "aliases the receiver method" do
+        expect(subject.example_receiver).to eq(receiver)
+      end
+    end
   end
 end
