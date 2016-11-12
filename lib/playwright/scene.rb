0d9a1bf5209d120775b55b3e7d2211e73b1a4792
Baylor Rae <baylor@thecodedeli.com>
2016-11-11 18:44:21 -0600

    change scene to a class instead of struct

--- lib/playwright/scene.rb
+++ lib/playwright/scene.rb
@@ -1,11 +1,23 @@
 module Playwright
-  Scene = Struct.new(:sender, :receiver) do
+  class Scene
+    attr_accessor :sender, :receiver
+
+    def initialize(sender, receiver)
+      @sender = sender
+      @receiver = receiver
+    end
+
     def self.sender_accessor(name)
       define_method(name, instance_method(:sender))
     end
 
     def self.receiver_accessor(name)
       define_method(name, instance_method(:receiver))
+    end
+
+    def ==(other)
+      return false unless other.is_a?(Scene)
+      sender == other.sender && receiver == other.receiver
     end
   end
 end
--- spec/playwright/scene_spec.rb
+++ spec/playwright/scene_spec.rb
@@ -23,5 +23,16 @@ module Playwright
         expect(subject.example_receiver).to eq(receiver)
       end
     end
+
+    context "==" do
+      it "compares sender and receiver" do
+        otherScene = ExampleScene.new(sender, receiver)
+        expect(subject == otherScene).to be_truthy
+      end
+
+      it "checks for same type" do
+        expect(subject == ExampleScene.new(1, 2)).to be_falsey
+      end
+    end
   end
 end
